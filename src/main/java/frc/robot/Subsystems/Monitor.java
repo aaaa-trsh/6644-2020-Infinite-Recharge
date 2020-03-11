0a876b1d4968e68cf80691de0b8affeefae07822
LucyFox <popbag4@gmail.com>
2020-03-10 23:00:45 -0700

    æugh
    Added Temp readins on analoug 0,1,2,3 to display the data on shuffleboard

--- src/main/java/frc/robot/Subsystems/Monitor.java
+++ src/main/java/frc/robot/Subsystems/Monitor.java
@@ -9,11 +9,34 @@ package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 public class Monitor{
-    AnalogInput Temp0 = new AnalogInput(0);
-    AnalogInput Temp1 = new AnalogInput(1);
-    AnalogInput Temp2 = new AnalogInput(2);
-    AnalogInput Temp3 = new AnalogInput(3);
-}
-class Temp0{
+    /// shit fuck oh god
+    /// shit fuck monitor temps
+    public static AnalogInput Temp0 = new AnalogInput(0);
+    public static AnalogInput Temp1 = new AnalogInput(1);
+    public static AnalogInput Temp2 = new AnalogInput(2);
+    public static AnalogInput Temp3 = new AnalogInput(3);
+    
 
+    public double getTemp0()
+    {
+        Temp0.setAverageBits(4);
+        return Temp0.getVoltage();
+    }
+    public double getTemp1()
+    {
+        Temp1.setAverageBits(4);
+        return Temp1.getVoltage();
+    }
+    public double getTemp2()
+    {
+        Temp2.setAverageBits(4);
+        return Temp2.getVoltage();
+    }
+    public double getTemp3()
+    {
+        Temp3.setAverageBits(4);
+        return Temp3.getVoltage();
+    }
 }
+
+/// takes values from analoug that are hooked to thermocouplers and sends them to shuffleboard - snups
